409362e0cbeabb9cffe9bd9dd886f1afb8960b87
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-09-13 17:16:20 -0700

    migrations complete

--- data/migrations/20190913100739_projects.js
+++ data/migrations/20190913100739_projects.js
@@ -1,22 +1,34 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('recipes', (table) => {
-        table.increments()
-        table.string('recipeName')
-        table.string('instructions')
+    return knex.schema
+        .createTable('project', (table) => {
+            table.increments()
+            table.string('projectName')
+            table.string('projectDescription')
+            })
+        .createTable('task', (table) => {
+            table.increments()
+            table.string('taskDescription')
+            table.string('taskNotes')
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('project')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
         })
-
-      .createTable('ingredients', (table) => {
-        table.increments()
-        table.string('ingredientName')
-        table.string('quantity')
-        table.integer('recipes_id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('recipes')
-            .onDelete('RESTRICT')
-            .onUpdate('CASCADE')
-      })
+        .createTable('resource', (table) => {
+            table.increments()
+            table.string('resourceName')
+            table.string('resourceDescription')
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('project')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
+        })
 };
 
 exports.down = function(knex, Promise) {
